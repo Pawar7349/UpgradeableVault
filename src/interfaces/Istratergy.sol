fd4f8320109951edf8b23d8880d145bf20c2312a
Pratik Pawar <pawarpratik7349@gmail.com>
2026-05-09 06:03:11 +0000

    project setup + foundry config

---
+++ src/interfaces/Istratergy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IStrategy {
+    function asset() external view returns (address);
+    function totalAssets() external view returns (uint256);
+    function deposit(uint256 amount) external;
+    function withdraw(uint256 amount) external returns (uint256);
+    function harvest() external;
+    function emergencyWithdraw() external;
+}
